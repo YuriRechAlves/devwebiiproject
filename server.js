6a984777ff3991dea8591ca5c0272262dab737bb
Ramão <ramao_tiago_tiburski@hotmail.com>
2023-06-28 15:25:46 -0300

    remoção de chaves

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = express()
 const port = process.env.PORT || 3000; 
 
 server.use(session({
-    secret: "V3r7r9$7q0d&p1!2$@PwC$3s8LpV",  //Chave secreta utilizada para assinar as sessões, garantindo a integridade e segurança das mesmas   
+    secret: process.env.SECRET,  //Chave secreta utilizada para assinar as sessões, garantindo a integridade e segurança das mesmas   
     resave: false, //Determina se a sessão deve ser regravada no armazenamento, mesmo que não tenha sido modificada
     saveUninitialized: true, //Define se a sessão deve ser salva no armazenamento, mesmo que não tenha sido modificada
     cookie: { maxAge: 10 * 60 * 1000 } //Define as configurações do cookie de sessão. maxAge está definido como 2 minutos (2 * 60 * 1000 milissegundos), especificando o tempo máximo de vida do cookie antes de expirar
